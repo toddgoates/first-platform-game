fb69df2bd026888069667d1beb40c1d83592b1b0
Todd Goates <toddgoates@gmail.com>
2025-11-20 22:26:46 -0700

    Update HUD. Working on concept of lives.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,9 +1,19 @@
 extends Node
 
 var coins = 0
+var lives = 3
 
 func add_coin():
 	coins += 1
-	var coin_label = get_tree().root.get_node("level1/CanvasLayer/CoinCounter")
+	var coin_label = get_tree().root.get_node("level1/HUD/CoinCounter")
 	coin_label.text = "COINS: " + str(coins)
 	print(coins)
+	
+func lose_life():
+	print("YOU LOST A LIFE!")
+	lives -= 1
+	var lives_label = get_tree().root.get_node("level1/HUD/LivesCounter")
+	lives_label.text = "LIVES: " + str(lives)
+	
+	if lives <= 0:
+		lives_label.text = "GAME OVER!"
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,9 +2,10 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 @onready var hurt_sound: AudioStreamPlayer2D = $HurtSound
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
-	print("YOU DIED!")
+	game_manager.lose_life()
 	hurt_sound.play()
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
